cfef12699183bc7ebad29f78516f7dc935c79cea
kareem-gafer10 <kareemgafer28.com>
2024-01-18 12:46:22 +0200

    handle error

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -1,14 +1,33 @@
 import axios from "axios";
+import Link from "next/link";
 
 const WebSearch = async ({ searchParams }) => {
   const { data } =
     await axios.get(`https://www.googleapis.com/customsearch/v1?key=
   AIzaSyC5AnILqdoHY9NOKe09QMSzrXNSMWLYCSE&cx=97b4948a6c9c14e02&q=${searchParams.searchTerm}`);
-  const result = data.items;
+  const results = data.items;
+
+
+  if (!results) {
+    return (
+      <div className="flex flex-col justify-center items-center pt-10">
+        <h1 className="text-3xl mb-4">No results found</h1>
+        <p className="text-lg">
+          Try searching for something else or go back to the homepage{" "}
+          <Link href="/" className="text-blue-500">
+            Home
+          </Link>
+        </p>
+      </div>
+    );
+  }
+
+
+
 
   return (
     <>
-      {result.map(({ title }) => (
+      {results&&results.map(({ title }) => (
         <h1 key={title}>{title}</h1>
       ))}
     </>
