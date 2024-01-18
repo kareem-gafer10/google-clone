bacd125c6389bf5268bff0c68436cd6cd8454426
kareem-gafer10 <kareemgafer28.com>
2024-01-18 12:28:04 +0200

    fetch data from google

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -1,8 +1,18 @@
+import axios from "axios";
 
-const WebSearch = () => {
+const WebSearch = async ({ searchParams }) => {
+  const { data } =
+    await axios.get(`https://www.googleapis.com/customsearch/v1?key=
+  AIzaSyC5AnILqdoHY9NOKe09QMSzrXNSMWLYCSE&cx=97b4948a6c9c14e02&q=${searchParams.searchTerm}`);
+  const result = data.items;
+
   return (
-    <div>WebSearch</div>
-  )
-}
+    <>
+      {result.map(({ title }) => (
+        <h1 key={title}>{title}</h1>
+      ))}
+    </>
+  );
+};
 
-export default WebSearch
+export default WebSearch;
