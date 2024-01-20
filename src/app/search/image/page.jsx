56f6a994a431d2e40a76ed77df50c5de44069217
kareem-gafer10 <kareemgafer28.com>
2024-01-20 07:32:33 +0200

    create img search result and display data

--- src/app/search/image/page.jsx
+++ src/app/search/image/page.jsx
@@ -1,8 +1,41 @@
 
-const ImageSearch = () => {
-  return (
-    <div>ImageSearch</div>
-  )
-}
 
-export default ImageSearch
+import ImageSearchResult from "@/components/ImageSearchResult";
+import axios from "axios";
+import Link from "next/link";
+
+const ImageSearch = async ({ searchParams }) => {
+  await new Promise((resolve) => setTimeout(resolve, 10000));
+  const { data } =
+    await axios.get(`https://www.googleapis.com/customsearch/v1?key=AIzaSyAcQ1MP-WEhaOpxn_m_t7infy3Yru-KnKs&cx=97b4948a6c9c14e02
+    &cx=97b4948a6c9c14e02&q=${searchParams.searchTerm}&searchType=image`);
+  const results = data.items;
+  
+
+  if (!results) {
+    return (
+      <div className="flex flex-col justify-center items-center pt-10">
+        <h1 className="text-3xl mb-4">No results found</h1>
+        <p className="text-lg">
+          Try searching for something else or go back to the homepage{" "}
+          <Link href="/" className="text-blue-500">
+            Home
+          </Link>
+        </p>
+      </div>
+    );
+  }
+
+
+
+
+
+
+  return (
+    <>
+     {results&&<ImageSearchResult results={data} />}
+    </>  
+  );
+};
+
+export default ImageSearch;
